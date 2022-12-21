49910fbfdf651fb0ecc6fb79107da04d038f4c43
DiegoDomingu3z <diegodominguezf40@gmail.com>
2022-12-20 23:06:46 -0700

    Completed d122022-dev-setup

---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import * as React from "react"
+
+
+const HomeScreen = () => {
+    return (
+        // Container to give screen entire view
+        <SafeAreaView>
+            <Text className='text-red-500'>THis is working</Text>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen;
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  // Add Content for tailwind to manipulate css 
+  content: ["./screens/**/*.{js,ts,jsx,tsx}"]
+}
+
+// NOTE add this later
+// "./components/**/*.{js,ts,jsx,tsx}",
